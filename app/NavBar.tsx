4402c1e03d2a5409ba30f4c1d1c2c883b2d9a77b
ace <aceveron@gmail.com>
2023-10-01 14:47:57 +0800

    database adapter

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,28 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <div className='flex bg-slate-200 p-3 space-x-3'>
       <Link href='/' className='mr-5'>
         Next.js
       </Link>
       <Link href='/users'>Users</Link>
-      <Link href='/api/auth/signin'>Login</Link>
+      {status === 'authenticated' && (
+        <div>
+          {session.user!.name}
+          <Link href='/api/auth/signout' className='ml-3'>
+            Sign Out
+          </Link>
+        </div>
+      )}
+      {status === 'unauthenticated' && (
+        <Link href='/api/auth/signin'>Login</Link>
+      )}
     </div>
   );
 };
